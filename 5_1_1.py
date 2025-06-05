2899cfd69ce974b88f7e2a26a9dff5b101aa28d4
Грачев Вадим <165307092+MAD17317@users.noreply.github.com>
2025-06-05 20:13:27 +0400

    Update 5_1_1.py

--- 5_1_1.py
+++ 5_1_1.py
@@ -1,6 +1,7 @@
 import time
+from itertools import permutations
 K = int(input("Введите количество членов комиссии (K): "))
-def generate_combinations():
+def algoritm():
     members = list(range(1, K + 1))
     print("\nВсе возможные варианты:")
     for chair in members:
@@ -8,8 +9,24 @@ def generate_combinations():
             if deputy != chair:
                 print(f"Председатель: {chair}, Заместитель: {deputy}")
     total = K * (K - 1)
-    end = time.time()
     print(f"\nВсего вариантов: {total}")
-    print(f"Время выполнения: {end - start:.5f} секунд")
-start = time.time()
-generate_combinations()
+start1 = time.time()
+algoritm()
+end1 = time.time()
+print(f"Время выполнения: {end1-start1:.5f} секунд")
+time1 = end1 - start1
+def function():
+    members = list(range(1, K + 1))
+    all_pairs = permutations(members, 2)
+    print("\nВсе возможные варианты:")
+    for chair, deputy in all_pairs:
+        print(f"Председатель: {chair}, Заместитель: {deputy}")
+    total = K * (K - 1)
+    print(f"\nВсего вариантов: {total}")
+start2 = time.time()
+function()
+end2 = time.time()
+print(f"Время выполнения: {end2-start2:.5f} секунд")
+time2 = end2 - start2
+if time1<time2: print('Алгоритмический способ быстрее')
+else: print('Алгоритмический способ дольше')
