176d5a1e1c7097eaf0dbc84459213984c8f69346
Грачев Вадим <165307092+MAD17317@users.noreply.github.com>
2025-06-05 18:20:05 +0400

    Update 5_1_1.py

--- 5_1_1.py
+++ 5_1_1.py
@@ -1,6 +1,6 @@
 import time
 K = int(input("Введите количество членов комиссии (K): "))
-def generate_chair_deputy_combinations():
+def generate_combinations():
     members = list(range(1, K + 1))
     print("\nВсе возможные варианты:")
     for chair in members:
@@ -8,8 +8,8 @@ def generate_chair_deputy_combinations():
             if deputy != chair:
                 print(f"Председатель: {chair}, Заместитель: {deputy}")
     total = K * (K - 1)
+    end = time.time()
     print(f"\nВсего вариантов: {total}")
+    print(f"Время выполнения: {end - start:.5f} секунд")
 start = time.time()
-generate_chair_deputy_combinations()
-end = time.time()
-print(f"Время выполнения: {end - start:.5f} секунд")
+generate_combinations()
