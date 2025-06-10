475c54518ccf42d1190dc4b49f9499dae3012f7f
Грачев Вадим <165307092+MAD17317@users.noreply.github.com>
2025-06-10 19:31:09 +0400

    Update 5_2.py

--- 5_2.py
+++ 5_2.py
@@ -4,27 +4,19 @@ import time
 a, filtered_pairs = [], []
 K = int(input("Введите количество членов комиссии (K): "))
 ratings = {i: random.randint(1, 10) for i in range(1, K + 1)}
+print(ratings)
 def first_part():
     members = list(ratings.keys())
-    for chair in members:
-        for deputy in members:
-            if chair != deputy:
-                if ratings[chair] - ratings[deputy] <= 3 and ratings[chair] - ratings[deputy] >= -3:
-                    filtered_pairs.append((chair, deputy))
-                    a.append(f"Председатель: {chair} (рейтинг {ratings[chair]}), Заместитель: {deputy} (рейтинг {ratings[deputy]})")
+    pairs_data = [(chair, deputy) for chair in members for deputy in members if chair != deputy and -3 < ratings[chair] - ratings[deputy] < 3]
     print("\nДопустимые варианты после фильтрации:")
-    for pair_info in a:
-        print(pair_info)
-    if filtered_pairs:
-        optimal_pair = max(filtered_pairs, key=lambda pair: ratings[pair[0]] + ratings[pair[1]])
-        chair, deputy = optimal_pair
-        total_rating = ratings[chair] + ratings[deputy]
-        end_time = time.time()
-        print(f"Оптимальная пара: Председатель {chair} ({ratings[chair]}), Заместитель {deputy} ({ratings[deputy]}), Общий рейтинг: {total_rating}")
-    else:print("\nНет допустимых пар.")
+    for chair, deputy in pairs_data:
+        print(f"Председатель: {chair} (рейтинг {ratings[chair]}), Заместитель: {deputy} (рейтинг {ratings[deputy]})")
+    optimal_pair = max(pairs_data, key=lambda pair: ratings[pair[0]] + ratings[pair[1]])
+    chair, deputy = optimal_pair
+    total_rating = ratings[chair] + ratings[deputy]
+    end_time = time.time()
+    print(f"Оптимальная пара: Председатель {chair} ({ratings[chair]}), Заместитель {deputy} ({ratings[deputy]}), Общий рейтинг: {total_rating}")
     print(f"Время выполнения: {end_time - start_time:.5f} секунд")
-
-
 def second_part():
     print("Рейтинги членов комиссии:", ratings)
     members = list(ratings.keys())
@@ -40,7 +32,6 @@ def second_part():
         end_time = time.time()
         print(f"\nОптимальная пара: Председатель {chair} (рейтинг {ratings[chair]}), Заместитель {deputy} (рейтинг {ratings[deputy]})")
         print(f"Сумма рейтингов: {total_rating}")
-    else: print("\nНет допустимых пар по заданным ограничениям.")
     print(f"Время выполнения: {end_time - start_time:.5f} секунд")
 start_time = time.time()
 first_part()
